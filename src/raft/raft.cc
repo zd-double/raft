f54146a2adb2b745ac24df85e604b77a501be384
zd-double <adikingustc@gmail.com>
2016-07-11 00:31:23 +0800

    first commit

---
+++ src/raft/raft.cc
@@ -0,0 +1,45 @@
+/***************************************************************************
+ *
+ * Copyright (c) 2016 Baidu All Rights Reserved
+ *
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file raft.cc
+ * @author Zhang Di(zhangdi05@baidu.com)
+ * @date 2016/07/10 00:29:31
+ * @version $Revision$
+ * @brief
+ *
+ **/
+
+#include <raft/raft.h>
+
+namespace raft
+{
+
+} // namespace raft
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set et ts=4 sw=4 sts=4 tw=100: */
---
+++ src/raft/raft.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+ *
+ * Copyright (c) 2016 Baidu All Rights Reserved
+ *
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file raft.h
+ * @author Zhang Di(zhangdi05@baidu.com)
+ * @date 2016/07/10 00:28:16
+ * @version $Revision$
+ * @brief
+ *
+ **/
+namespace raft
+{
+
+} // namespace raft
+
+#ifndef  __RAFT_H_
+#define  __RAFT_H_
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif  //__RAFT_H_
+
+/* vim: set et ts=4 sw=4 sts=4 tw=100: */
